3c2aa87f24a93df38a1779ccc5e08755aa150635
manhinhang <manhinhang@gmail.com>
2025-06-22 01:13:09 +0800

    Refactor Dockerfile and scripts for improved user permissions and health checks
    - Changed package installation command in Dockerfile from `apt install` to `apt-get install` for consistency.
    - Added a non-root user and adjusted permissions for necessary directories and scripts to enhance security.
    - Implemented a health check in the Dockerfile to monitor the FutuOpenD process.
    - Updated the start script to ensure proper quoting and path adjustments for the FutuOpenD XML file.

--- script/start.sh
+++ script/start.sh
@@ -2,7 +2,7 @@
 
 FUTU_OPEND_RSA_FILE_PATH=/.futu/futu.pem
 FUTU_OPEND_IP=${FUTU_OPEND_IP:-$(cat /etc/hostname)}
-FUTU_ACCOUNT_PWD_MD5=$(echo -n $FUTU_ACCOUNT_PWD | md5sum | awk '{print $1}')
+FUTU_ACCOUNT_PWD_MD5=$(echo -n "$FUTU_ACCOUNT_PWD" | md5sum | awk '{print $1}')
 
 echo "FUTO_ACCOUNT_ID: $FUTU_ACCOUNT_ID"
 echo "FUTU_OPEND_RSA_FILE_PATH: $FUTU_OPEND_RSA_FILE_PATH"
@@ -20,7 +20,7 @@ sed -i "s|<login_pwd_md5>.*<\/login_pwd_md5>|<login_pwd_md5>$FUTU_ACCOUNT_PWD_MD
 sed -i "s|<rsa_private_key>.*<\/rsa_private_key>|<rsa_private_key>$FUTU_OPEND_RSA_FILE_PATH</rsa_private_key>|" $FUTU_OPEND_XML_PATH
 
 # if telnet is available, then check if the port is open
-if [ -n $FUTU_OPEND_TELNET_PORT ]; then
+if [ -n "$FUTU_OPEND_TELNET_PORT" ]; then
 	sed -i "s|<telnet_port>.*<\/telnet_port>|<telnet_port>$FUTU_OPEND_TELNET_PORT</telnet_port>|" $FUTU_OPEND_XML_PATH
 fi
 
