9260db9599c3eb255b0c2480a3238be46317debc
manhinhang <manhinhang@gmail.com>
2026-03-07 10:20:28 +0800

    Fix permission denied error in start.sh
    - Copy FutuOpenD.xml to /tmp before modifying
    - Run FutuOpenD with -config flag pointing to modified file
    - Allows non-root user to modify config without permission errors

--- script/start.sh
+++ script/start.sh
@@ -8,10 +8,12 @@ echo "FUTO_ACCOUNT_ID: $FUTU_ACCOUNT_ID"
 echo "FUTU_OPEND_RSA_FILE_PATH: $FUTU_OPEND_RSA_FILE_PATH"
 echo "FUTU_OPEND_IP: $FUTU_OPEND_IP"
 
-# sh /bin/update_futu_xml.sh $FUTU_ACCOUNT_ID $FUTU_ACCOUNT_PWD $FUTU_OPEND_RSA_FILE_PATH
-FUTU_OPEND_XML_PATH=/bin/FutuOpenD.xml
+FUTU_OPEND_XML_SRC=/bin/FutuOpenD.xml
+FUTU_OPEND_XML_PATH=/tmp/FutuOpenD.xml
 
-echo "Replace FutuOpenD.xml"
+echo "Copy and configure FutuOpenD.xml"
+
+cp "$FUTU_OPEND_XML_SRC" "$FUTU_OPEND_XML_PATH"
 
 sed -i "s|<ip>.*<\/ip>|<ip>$FUTU_OPEND_IP</ip>|" $FUTU_OPEND_XML_PATH
 sed -i "s|<api_port>.*<\/api_port>|<api_port>$FUTU_OPEND_PORT</api_port>|" $FUTU_OPEND_XML_PATH
@@ -19,12 +21,10 @@ sed -i "s|<login_account>.*<\/login_account>|<login_account>$FUTU_ACCOUNT_ID</lo
 sed -i "s|<login_pwd_md5>.*<\/login_pwd_md5>|<login_pwd_md5>$FUTU_ACCOUNT_PWD_MD5</login_pwd_md5>|" $FUTU_OPEND_XML_PATH
 sed -i "s|<rsa_private_key>.*<\/rsa_private_key>|<rsa_private_key>$FUTU_OPEND_RSA_FILE_PATH</rsa_private_key>|" $FUTU_OPEND_XML_PATH
 
-# Enable telnet port if specified
 if [ -n "$FUTU_OPEND_TELNET_PORT" ]; then
 	sed -i "s|###FUTU_OPEND_TELNET_PORT###|$FUTU_OPEND_TELNET_PORT|" $FUTU_OPEND_XML_PATH
 else
-	# Comment out telnet port if not specified
 	sed -i "s|<telnet_port>.*<\/telnet_port>|<!-- <telnet_port>22222</telnet_port> -->|" $FUTU_OPEND_XML_PATH
 fi
 
-/bin/FutuOpenD
+/bin/FutuOpenD -config=$FUTU_OPEND_XML_PATH
